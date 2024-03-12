2c659ae2ef225e47115fc208ef236077f9e95f78
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-03-12 16:30:50 -0300

    feat: add drum-kit folder

---
+++ drum-kit/index.js
@@ -0,0 +1,49 @@
+document.querySelectorAll(".drum").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    makeSound(event.target.innerHTML);
+    buttonAnimation(event.target.innerHTML);
+  });
+});
+
+document.addEventListener("keydown", (event) => {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+    default:
+      console.error("Button does not exists");
+  }
+}
+
+function buttonAnimation(currentKey) {
+  const activeButton = document.querySelector(`.${currentKey}`);
+  activeButton.classList.add("pressed");
+
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 100);
+  
+}
