7c1157f77af4582d8ab6b6e9468c9afd599ad6c4
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-03-14 15:25:31 -0300

    feat: add express folder

---
+++ express/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+// Use Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan("combined"));
+
+// Create Middleware
+app.use((req, res, next) => {
+  console.log("Request Method: " + req.method);
+  console.log("Request URL: " + req.url);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  const bandName = req.body["street"] + req.body["pet"];
+  res.send(`<h1>Your band name is: </h1><h2>${bandName}</h2>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port} 🔥`);
+});
